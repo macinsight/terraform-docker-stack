c6430aac8e100b1b95143e747e371c3da8e6ca98
macinsight <gh@macinsight.net>
2024-07-26 19:25:29 +0200

    Create DB

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ terraform {
       source = "hashicorp/template"
       version = "~> 2.0"
     }
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.22.0"
+    }
   }
 }
 
--- providers.tf
+++ providers.tf
@@ -1,6 +1,12 @@
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+
 provider "cloudflare" {
   # Token pulled via ENV
+}
+
+provider "postgresql" {
+  host = docker_container.postgres.name
+
 }
