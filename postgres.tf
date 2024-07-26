368fc1c26400adacce2dc69e112e6a6467452644
macinsight <gh@macinsight.net>
2024-07-26 19:12:32 +0200

    Create DB

--- postgres.tf
+++ postgres.tf
@@ -40,9 +40,9 @@ resource "null_resource" "initialize_postgresql" {
     command = "bash postgresql_init.sh"
     environment = {
       DB_HOST        = "localhost"
-      POSTGRES_USER  = var.DB_USER
-      POSTGRES_PASSWORD = var.DB_PASSWORD
-      DB_NAME        = var.DB_NAME
+      POSTGRES_USER  = var.POSTGRES_USER
+      POSTGRES_PASSWORD = var.POSTGRES_PASSWORD
+      DB_NAME        = var.POSTGRES_DB
     }
   }
 }
