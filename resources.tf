783b1034075315943ced694539d6f04d42cbd0e3
macinsight <gh@macinsight.net>
2024-07-26 13:57:55 +0200

    traefik

--- resources.tf
+++ resources.tf
@@ -5,4 +5,18 @@ resource "docker_image" "netbox" {
 resource "docker_container" "netbox" {
   image = docker_image.netbox.image_id
   name  = "netbox"
+  
+  ports {
+    internal = 80
+  }
+
+  labels = {
+    "traefik.enable"                      = "true"
+    "traefik.http.routers.netbox.rule"    = "Host(\"${docker_container.netbox.name}.macinsight.io\")"
+    "traefik.http.services.netbox.loadbalancer.server.port" = "80"
+  }
+
+  networks_advanced {
+    name = docker_network.traefik_network.name
+  }
 }
---
+++ traefik.tf
@@ -0,0 +1,32 @@
+resource "docker_network" "traefik_network" {
+  name = "traefik"
+}
+
+resource "docker_container" "traefik" {
+  name  = "traefik"
+  image = "traefik:v2.5"
+  restart = "always"
+
+  ports {
+    internal = 80
+    external = 80
+  }
+
+  ports {
+    internal = 8080
+    external = 8080
+  }
+
+  volumes {
+    volume_name    = "traefik"
+    container_path = "/etc/traefik/traefik.yml"
+  }
+
+  labels = {
+    "traefik.enable" = "true"
+  }
+
+  networks_advanced {
+    name = docker_network.traefik_network.name
+  }
+}
